bd06183472b8aa7d6a75b16c2a364af2154f2bdd
Siraaj Khandkar <siraaj@khandkar.net>
2015-11-20 16:57:11 -0500

    Remove defaults from pool spec record definition.

--- include/ibrowse_pool_spec.hrl
+++ include/ibrowse_pool_spec.hrl
@@ -1,8 +1,8 @@
 -record(ibrowse_pool_spec,
-    { name                   :: atom()
-    , ssl_opts          = [] :: [term()]
-    , max_sessions      = 10 :: pos_integer()
-    , max_pipeline_size = 10 :: pos_integer()
-    , max_attempts      = 3  :: pos_integer()
-    , timeout           = 10000 :: timeout()
+    { name              :: atom()
+    , ssl_opts          :: [term()]
+    , max_sessions      :: pos_integer()
+    , max_pipeline_size :: pos_integer()
+    , max_attempts      :: pos_integer()
+    , timeout           :: timeout()
     }).
